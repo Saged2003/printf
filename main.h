b63322d6001d5d748319285dcf50acea81a34398
Saged2003 <sagedryan775@gmail.com>
2023-10-15 06:58:29 -0700

    new

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef UNTITLED2_MAIN_H
+#define UNTITLED2_MAIN_H
+#include <stdio.h>
+#include <stdrg.h>
+#include <unistd.h>
+#include <stdlib.h>
+int kind(char c, va_list args);
+int _printf(const char *format, ...);
+#endif
---
+++ print.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _printf - it's the base boooyy
+ * @format: pointer to the input
+ * Return: the size.
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+	int count = 0;
+	char c;
+	char *s;
+
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			count += kind(format[i], args);
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
+
+/**
+ * _printf - it's the base boooyy
+ * @format: pointer to the input
+ * Return: the size.
+*/
+
+
+int kind(char c, va_list args)
+{
+
+	int nona = 0, i = 0;
+	char *st, a;
+
+	if (c == '%')
+		nona += _putchar('%');
+	else if (c == 's')
+	{
+		st = va_arg(args, char *);
+		while (st[i])
+		{
+			nona += _putchar(st[i]);
+			i++;
+		}
+		
+	}
+	else if (c == 'c')
+	{
+		a = va_arg(args, int);
+		nona += _putchar(a);
+	}
+	return (nona);
+}
